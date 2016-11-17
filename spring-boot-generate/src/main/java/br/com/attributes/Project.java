616c574bf8ed8070c4c0ff41b2669ca2bf814b43
NetoDevelm <josevieira.dev@gmail.com>
2016-11-17 08:42:49 -0200

    bug new project in linux solved

---
+++ spring-boot-generate/src/main/java/br/com/attributes/Project.java
@@ -0,0 +1,64 @@
+package br.com.attributes;
+
+/**
+ * Created by josevieira on 11/17/16.
+ */
+public class Project {
+
+    private String srcMainJava;
+    private String srcMainResources;
+    private String srcTestJava;
+    private String srcTestResources;
+    private String target;
+
+    public Project(){
+    }
+
+    public Project(String srcMainJava, String srcMainResources, String srcTestJava, String srcTestResources, String target) {
+        this.srcMainJava = srcMainJava;
+        this.srcMainResources = srcMainResources;
+        this.srcTestJava = srcTestJava;
+        this.srcTestResources = srcTestResources;
+        this.target = target;
+    }
+
+    public String getSrcMainJava() {
+        return srcMainJava;
+    }
+
+    public void setSrcMainJava(String srcMainJava) {
+        this.srcMainJava = srcMainJava;
+    }
+
+    public String getSrcMainResources() {
+        return srcMainResources;
+    }
+
+    public void setSrcMainResources(String srcMainResources) {
+        this.srcMainResources = srcMainResources;
+    }
+
+    public String getSrcTestJava() {
+        return srcTestJava;
+    }
+
+    public void setSrcTestJava(String srcTestJava) {
+        this.srcTestJava = srcTestJava;
+    }
+
+    public String getSrcTestResources() {
+        return srcTestResources;
+    }
+
+    public void setSrcTestResources(String srcTestResources) {
+        this.srcTestResources = srcTestResources;
+    }
+
+    public String getTarget() {
+        return target;
+    }
+
+    public void setTarget(String target) {
+        this.target = target;
+    }
+}
---
+++ spring-boot-generate/src/main/java/br/com/util/DirectorysLinux.java
@@ -0,0 +1,14 @@
+package br.com.util;
+
+/**
+ * Created by josevieira on 11/17/16.
+ */
+public class DirectorysLinux {
+
+    public static final String SRC_MAIN_JAVA = "//src//main//java";
+    public static final String SRC_MAIN_RESOURCES = "//src//main//resources";
+    public static final String SRC_TEST_JAVA = "//src//test//java";
+    public static final String SRC_TEST_RESOURCES = "//src//test//resources";
+    public static final String TARGET = "//target";
+
+}
