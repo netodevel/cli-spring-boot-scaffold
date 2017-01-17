64dfffc02e882a80f1d6e81bde238ef1931a4361
José Vieira da Silva Neto <josevieira.dev@gmail.com>
2017-01-17 14:30:30 -0300

    revised

--- spring-boot-generate/src/main/java/br/com/generate/DependencyVersion.java
+++
@@ -1,8 +0,0 @@
-package br.com.generate;
-
-public class DependencyVersion {
-	
-	public static final String SPRING_BOOT_VERSION = "1.4.1.RELEASE";
-	public static final String KOTLIN_VERSION = "1.0.4";
-	
-}
