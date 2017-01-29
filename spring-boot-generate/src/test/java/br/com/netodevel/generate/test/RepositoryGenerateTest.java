e788221d13a679e3103f80586dd1950cc3799857
NetoDevelm <josevieira.dev@gmail.com>
2017-01-29 15:00:59 -0200

    #28 test case repository generator.

--- spring-boot-generate/src/test/java/br/com/netodevel/generate/test/RepositoryGenerateTest.java
+++ spring-boot-generate/src/test/java/br/com/netodevel/generate/test/RepositoryGenerateTest.java
@@ -15,28 +15,37 @@ import br.com.generate.java.command.repository.RepositoryGenerateJava;
 import br.com.netodevel.generate.utils.FileGeneratorTestUtils;
 
 public class RepositoryGenerateTest {
-	
+
 	File repositoryGeneratorFile;
 	File convertRepositoryToText;
-	
+
 	@Before
 	public void setUp() throws IOException {
 		new RepositoryGenerateJava().generate("User", null, "template-repository.txt");
 		repositoryGeneratorFile = new File("src/main/java/br/com/example/repository/UserRepository.java");
 		convertRepositoryToText = FileGeneratorTestUtils.convertJavaToText(repositoryGeneratorFile, Layers.REPOSITORY, "UserRepositoryTest.txt");
-		
 	}
-	
+
 	@Test
 	public void testGenerateRepository() throws IOException {
 		boolean validateFileEquals = FileUtils.contentEquals(convertRepositoryToText, new File("src/test/resources/templates/java/repository/UserRepository.txt"));
 		assertEquals("should be true", true, validateFileEquals);
 	}
-	
+
+	@Test
+	public void testValidateFileExists() throws IOException {
+		java.io.ByteArrayOutputStream out = new java.io.ByteArrayOutputStream();
+		System.setOut(new java.io.PrintStream(out));
+		new RepositoryGenerateJava().generate("User", null, "template-repository.txt");
+		String outPutExpected = "Error: file UserRepository.java already exists.";
+		assertEquals("should be true", outPutExpected.trim(), out.toString().trim());
+
+	}
+
 	@After
 	public void tearDown() throws IOException {
 		FileGeneratorTestUtils.deleteFileAndDirectory(repositoryGeneratorFile, convertRepositoryToText);
 	}
-	
-	
+
+
 }
