9be20198918dffefeea5aaa6867e7e8a22b7bbd2
NetoDevelm <josevieira.dev@gmail.com>
2017-01-29 10:49:31 -0200

    repository generator test finalized.

--- spring-boot-generate/src/test/java/br/com/netodevel/generate/test/RepositoryGenerateTest.java
+++ spring-boot-generate/src/test/java/br/com/netodevel/generate/test/RepositoryGenerateTest.java
@@ -7,35 +7,36 @@ import java.io.IOException;
 
 import org.apache.commons.io.FileUtils;
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import br.com.generate.Layers;
 import br.com.generate.java.command.repository.RepositoryGenerateJava;
-import br.com.netodevel.generate.utils.ConvertFileUtils;
+import br.com.netodevel.generate.utils.FileGeneratorTestUtils;
 
 public class RepositoryGenerateTest {
 	
+	File repositoryGeneratorFile;
+	File convertRepositoryToText;
+	
+	@Before
+	public void setUp() throws IOException {
+		new RepositoryGenerateJava().generate("User", null, "template-repository.txt");
+		repositoryGeneratorFile = new File("src/main/java/br/com/example/repository/UserRepository.java");
+		convertRepositoryToText = FileGeneratorTestUtils.convertJavaToText(repositoryGeneratorFile, Layers.REPOSITORY, "UserRepositoryTest.txt");
+		
+	}
 	
 	@Test
 	public void testGenerateRepository() throws IOException {
-		new RepositoryGenerateJava().generate("User", null, "template-repository.txt");
-		File repositoryJavaFile = new File("src/main/java/br/com/example/repository/UserRepository.java");
-	
-		File newTextFile = ConvertFileUtils.convertJavaToText(repositoryJavaFile, Layers.REPOSITORY, "UserRepositoryTest.txt");
-		
-		boolean validateFileEquals = FileUtils.contentEquals(newTextFile, new File("src/test/resources/templates/java/repository/UserRepository.txt"));
+		boolean validateFileEquals = FileUtils.contentEquals(convertRepositoryToText, new File("src/test/resources/templates/java/repository/UserRepository.txt"));
 		assertEquals("should be true", true, validateFileEquals);
 	}
 	
 	@After
-	public void tearDown() {
-		File fileJava = new File("src/main/java/br/com/example/repository/UserRepository.java");
-		File fileText = new File("src/test/resources/templates/java/repository/UserRepositoryTest.txt");
-
-//		FileUtils.deleteDirectory("src/main/java/br/com/example/repository");
-		
-		fileJava.delete();
-		fileText.delete();
+	public void tearDown() throws IOException {
+		FileGeneratorTestUtils.deleteFileAndDirectory(repositoryGeneratorFile, convertRepositoryToText);
 	}
+	
 	
 }
--- spring-boot-generate/src/test/java/br/com/netodevel/generate/utils/FileGeneratorTestUtils.java
+++ spring-boot-generate/src/test/java/br/com/netodevel/generate/utils/FileGeneratorTestUtils.java
@@ -9,7 +9,7 @@ import java.io.InputStream;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 
-public class ConvertFileUtils {
+public class FileGeneratorTestUtils {
 
 	public static File convertJavaToText(File file, String layer, String outPutFileName) throws FileNotFoundException, IOException {
 		InputStream in =  new FileInputStream(file);
@@ -17,6 +17,13 @@ public class ConvertFileUtils {
 		File newTextFile = new File("src/test/resources/templates/java/" + layer + "/" + outPutFileName);
 		FileUtils.writeStringToFile(newTextFile, theString);
 		return newTextFile;
+	}
+	
+	public static void deleteFileAndDirectory(File... files) throws IOException {
+		for (File file : files) {
+			file.delete();
+		}
+		FileUtils.deleteDirectory(new File("src/main/java/br/com/example"));
 	}
 	
 }
